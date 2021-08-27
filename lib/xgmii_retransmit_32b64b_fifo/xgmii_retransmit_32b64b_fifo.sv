fde8b3d863019ba8899809806b257927cb33ec40
0xDEBB20E3 <0xDEBB20E3@gmail.com>
2021-08-27 10:54:10 +0300

    xgmii_retransmit_32b64b_fifo fix

--- lib/xgmii_retransmit_32b64b_fifo/xgmii_retransmit_32b64b_fifo.sv
+++ lib/xgmii_retransmit_32b64b_fifo/xgmii_retransmit_32b64b_fifo.sv
@@ -3,12 +3,10 @@ import gtype::xgmii64_t;
 
 module xgmii_retransmit_32b64b_fifo
 (
-    input wire clk_32,
-    input wire rst_32,
-    input wire clk_64,
-    input wire rst_64,
-    input xgmii32_t xgmii32,
-    output xgmii64_t xgmii64
+    input wire clk_rx,
+    input wire clk_tx,
+    input  xgmii32_t rx,
+    output xgmii64_t tx
 );
 
 wire [79:0] fifo_xgmii_retransmit_64b64b_data_w    ;
@@ -19,37 +17,36 @@ wire        fifo_xgmii_retransmit_64b64b_wrreq_w   ;
 wire [79:0] fifo_xgmii_retransmit_64b64b_q_w       ;
 wire        fifo_xgmii_retransmit_64b64b_rdempty_w ;
 wire        fifo_xgmii_retransmit_64b64b_wrfull_w  ;
-wire [8:0]  fifo_xgmii_retransmit_64b64b_rdusedw_w ;
+wire [9:0]  fifo_xgmii_retransmit_64b64b_rdusedw_w ;
 
-wire        start_w = xgmii32.ena &  (xgmii32.ctrl==4'b0001 & xgmii32.data[7:0]==8'hFB);
+wire        start_w = rx.ena &  (rx.ctrl==4'b0001 & rx.data[7:0]==8'hFB);
 
-wire        state_w = xgmii32.ena & ((xgmii32.ctrl==4'b0001 & xgmii32.data[7:0]==8'hFB) | //start
-                                     (xgmii32.ctrl==4'b0000                           ) | //data 
-                                     (xgmii32.ctrl==4'b1111 & xgmii32.data[7:0]==8'hFD) | //stop
-                                     (xgmii32.ctrl==4'b1110                           ) | //stop
-                                     (xgmii32.ctrl==4'b1100                           ) | //stop
-                                     (xgmii32.ctrl==4'b1000                           ) );//stop
+wire        state_w = rx.ena & ((rx.ctrl==4'b0001 & rx.data[7:0]==8'hFB) | //start
+                                (rx.ctrl==4'b0000                      ) | //data 
+                                (rx.ctrl==4'b1111 & rx.data[7:0]==8'hFD) | //stop
+                                (rx.ctrl==4'b1110                      ) | //stop
+                                (rx.ctrl==4'b1100                      ) | //stop
+                                (rx.ctrl==4'b1000                      ) );//stop
 
-xgmii32_t     xgmii32_r;
-reg           cnt_32_r = 0;
-reg           state_0r = 0;
-reg           state_1r = 0;
+xgmii32_t rx_r;
+reg       cnt_32_r = 0;
+reg       state_0r = 0;
+reg       state_1r = 0;
 
-always @(posedge clk_32) if (xgmii32.ena) xgmii32_r <= xgmii32;
-always @(posedge clk_32) if (xgmii32.ena) state_0r  <= state_w;
-always @(posedge clk_32) if (xgmii32.ena) state_1r  <= state_0r;
+always @(posedge clk_rx) if (rx.ena) rx_r <= rx;
+always @(posedge clk_rx) if (rx.ena) state_0r  <= state_w;
+always @(posedge clk_rx) if (rx.ena) state_1r  <= state_0r;
 
-always @(posedge clk_32) begin
-    if      (rst_32 )     begin cnt_32_r <= 0;          end
-    else if (start_w)     begin cnt_32_r <= 1;          end
-    else if (xgmii32.ena) begin cnt_32_r <= !cnt_32_r;  end
+always @(posedge clk_rx) begin
+    if      (start_w) begin cnt_32_r <= 1;          end
+    else if (rx.ena)  begin cnt_32_r <= !cnt_32_r;  end
 end
 
 ////////////////////////////////////////////////////////////////////////////////
-assign fifo_xgmii_retransmit_64b64b_wrclk_w  = clk_32; 
-assign fifo_xgmii_retransmit_64b64b_wrreq_w  = cnt_32_r & !start_w & xgmii32.ena & (state_w | state_0r | state_1r);
-assign fifo_xgmii_retransmit_64b64b_data_w   = {xgmii32.ctrl,xgmii32_r.ctrl,xgmii32.data,xgmii32_r.data};
-assign fifo_xgmii_retransmit_64b64b_rdclk_w  = clk_64;
+assign fifo_xgmii_retransmit_64b64b_wrclk_w  = clk_rx; 
+assign fifo_xgmii_retransmit_64b64b_wrreq_w  = cnt_32_r & !start_w & rx.ena & (state_w | state_0r | state_1r);
+assign fifo_xgmii_retransmit_64b64b_data_w   = {8'd0,rx.ctrl,rx_r.ctrl,rx.data,rx_r.data};
+assign fifo_xgmii_retransmit_64b64b_rdclk_w  = clk_tx;
 assign fifo_xgmii_retransmit_64b64b_rdreq_w  = !fifo_xgmii_retransmit_64b64b_rdempty_w;
 
 fifo_xgmii_retransmit_64b64b fifo_xgmii_retransmit_64b64b_u 
@@ -66,8 +63,8 @@ fifo_xgmii_retransmit_64b64b fifo_xgmii_retransmit_64b64b_u
 );
 ////////////////////////////////////////////////////////////////////////////////
 
-assign xgmii64.data = fifo_xgmii_retransmit_64b64b_q_w[63:0];
-assign xgmii64.ctrl = fifo_xgmii_retransmit_64b64b_q_w[71:64];
-assign xgmii64.ena  = fifo_xgmii_retransmit_64b64b_rdreq_w;
+assign tx.data = fifo_xgmii_retransmit_64b64b_q_w[63:0];
+assign tx.ctrl = fifo_xgmii_retransmit_64b64b_q_w[71:64];
+assign tx.ena  = fifo_xgmii_retransmit_64b64b_rdreq_w;
 
 endmodule
--- top/simple10GbaseR.sv
+++ top/simple10GbaseR.sv
@@ -1,4 +1,5 @@
 import gtype::xgmii32_t;
+import gtype::xgmii64_t;
 
 module simple10GbaseR
 #(
@@ -11,7 +12,7 @@ module simple10GbaseR
     output wire serial_tx [NUM_TR-1:0]
 );
     
-localparam TYPE_TR[NUM_TR-1:0] = {"BASER","PMA","PMA"};
+localparam [NUM_TR-1:0] TYPE_TR_PMA = {1'b0,1'b1,1'b1};
 
 genvar i;
 
@@ -75,11 +76,18 @@ xgmii32_t xgmii_retransmit_32b32b_fifo_0to0_rx_w;
 wire      xgmii_retransmit_32b32b_fifo_0to0_clk_tx_w;
 xgmii32_t xgmii_retransmit_32b32b_fifo_0to0_tx_w;    
 xgmii32_t xgmii_retransmit_32b32b_fifo_0to0_tx_r;    
+
 wire      xgmii_retransmit_32b32b_fifo_0to1_clk_rx_w;
 xgmii32_t xgmii_retransmit_32b32b_fifo_0to1_rx_w;    
 wire      xgmii_retransmit_32b32b_fifo_0to1_clk_tx_w;
 xgmii32_t xgmii_retransmit_32b32b_fifo_0to1_tx_w;    
 xgmii32_t xgmii_retransmit_32b32b_fifo_0to1_tx_r;    
+
+wire      xgmii_retransmit_32b64b_fifo_0to2_clk_rx_w;
+xgmii32_t xgmii_retransmit_32b64b_fifo_0to2_rx_w;    
+wire      xgmii_retransmit_32b64b_fifo_0to2_clk_tx_w;
+xgmii64_t xgmii_retransmit_32b64b_fifo_0to2_tx_w;    
+xgmii64_t xgmii_retransmit_32b64b_fifo_0to2_tx_r;    
 
 wire clk_glbl_w;
 wire rst_glbl_w;
@@ -89,7 +97,7 @@ wire xgmii_rx_clk_w[NUM_TR-1:0];
 wire xgmii_tx_clk_w[NUM_TR-1:0];
 
 //////////////////////////////////////////////////////////////////////////
-assign clk_rst_tr_fpll_powerdown_w = tr_pma_wrapper_tr_fpll_pll_powerdown_w[1] | tr_pma_wrapper_tr_fpll_pll_powerdown_w[0];
+assign clk_rst_tr_fpll_powerdown_w = tr_baser_wrapper_tr_fpll_pll_powerdown_w[2] | tr_pma_wrapper_tr_fpll_pll_powerdown_w[1] | tr_pma_wrapper_tr_fpll_pll_powerdown_w[0];
 
 clk_rst clk_rst_u
 (
@@ -109,7 +117,7 @@ clk_rst clk_rst_u
 
 //////////////////////////////////////////////////////////////////////////
 for (i=0;i<NUM_TR;i=i+1) begin : TR
-    if (TYPE_TR[i]=="PMA") begin : PMA
+    if (TYPE_TR_PMA[i]==1'b1) begin : PMA
         assign tr_pma_wrapper_clk_glbl_w              [i]= clk_glbl_w              ;
         assign tr_pma_wrapper_rst_glbl_w              [i]= rst_glbl_w              ;
         assign tr_pma_wrapper_refclk_w                [i]= ref_clk                 ;
@@ -144,7 +152,7 @@ for (i=0;i<NUM_TR;i=i+1) begin : TR
         assign xgmii_tx_clk_w [i] = tr_pma_wrapper_pma_tx_clk_w[i];
         assign serial_tx      [i] = tr_pma_wrapper_tx_serial_w [i];
     end
-    else if (TYPE_TR[i]=="BASER") begin : BASER
+    else begin : BASER
         assign tr_baser_wrapper_clk_glbl_w              [i]= clk_glbl_w;
         assign tr_baser_wrapper_rst_glbl_w              [i]= rst_glbl_w;
         assign tr_baser_wrapper_clk_156_w               [i]= clk_156_w;
@@ -154,30 +162,30 @@ for (i=0;i<NUM_TR;i=i+1) begin : TR
         assign tr_baser_wrapper_tr_fpll_pll_locked_w    [i]= clk_rst_tr_fpll_pll_locked_w    ;
         assign tr_baser_wrapper_tr_fpll_tx_serial_clk_w [i]= clk_rst_tr_fpll_tx_serial_clk_w ;
         assign tr_baser_wrapper_tr_fpll_pll_cal_busy_w  [i]= clk_rst_tr_fpll_pll_cal_busy_w  ;
-        assign tr_baser_wrapper_xgmii_tx_w              [i]= ;
+        assign tr_baser_wrapper_xgmii_tx_w              [i]= xgmii_retransmit_32b64b_fifo_0to2_tx_r;
 
         tr_baser_wrapper tr_baser_wrapper_u
         (
-            .clk_glbl              ( tr_baser_wrapper_clk_glbl_w             ),//input
-            .rst_glbl              ( tr_baser_wrapper_rst_glbl_w             ),//input
-            .clk_156               ( tr_baser_wrapper_clk_156_w              ),//input
-            .rst_156               ( tr_baser_wrapper_rst_156_w              ),//input
-            .refclk                ( tr_baser_wrapper_refclk_w               ),//input
-            .rx_serial             ( tr_baser_wrapper_rx_serial_w            ),//input
-            .tx_serial             ( tr_baser_wrapper_tx_serial_w            ),//output
-            .tr_fpll_pll_powerdown ( tr_baser_wrapper_tr_fpll_pll_powerdown_w),//output
-            .tr_fpll_pll_locked    ( tr_baser_wrapper_tr_fpll_pll_locked_w   ),//input
-            .tr_fpll_tx_serial_clk ( tr_baser_wrapper_tr_fpll_tx_serial_clk_w),//input
-            .tr_fpll_pll_cal_busy  ( tr_baser_wrapper_tr_fpll_pll_cal_busy_w ),//input
-            .xgmii_tx              ( tr_baser_wrapper_xgmii_tx_w             ),//input
-            .xgmii_tx_rdy          ( tr_baser_wrapper_xgmii_tx_rdy_w         ),//output
-            .xgmii_tx_clk          ( tr_baser_wrapper_xgmii_tx_clk_w         ),//output
-            .xgmii_tx_rst          ( tr_baser_wrapper_xgmii_tx_rst_w         ),//output
-            .xgmii_rx              ( tr_baser_wrapper_xgmii_rx_w             ),//output
-            .xgmii_rx_rdy          ( tr_baser_wrapper_xgmii_rx_rdy_w         ),//output
-            .xgmii_rx_clk          ( tr_baser_wrapper_xgmii_rx_clk_w         ),//output
-            .xgmii_rx_rst          ( tr_baser_wrapper_xgmii_rx_rst_w         ),//output
-            .rx_sync               ( tr_baser_wrapper_rx_sync_w              ) //output
+            .clk_glbl              ( tr_baser_wrapper_clk_glbl_w             [i]),//input
+            .rst_glbl              ( tr_baser_wrapper_rst_glbl_w             [i]),//input
+            .clk_156               ( tr_baser_wrapper_clk_156_w              [i]),//input
+            .rst_156               ( tr_baser_wrapper_rst_156_w              [i]),//input
+            .refclk                ( tr_baser_wrapper_refclk_w               [i]),//input
+            .rx_serial             ( tr_baser_wrapper_rx_serial_w            [i]),//input
+            .tx_serial             ( tr_baser_wrapper_tx_serial_w            [i]),//output
+            .tr_fpll_pll_powerdown ( tr_baser_wrapper_tr_fpll_pll_powerdown_w[i]),//output
+            .tr_fpll_pll_locked    ( tr_baser_wrapper_tr_fpll_pll_locked_w   [i]),//input
+            .tr_fpll_tx_serial_clk ( tr_baser_wrapper_tr_fpll_tx_serial_clk_w[i]),//input
+            .tr_fpll_pll_cal_busy  ( tr_baser_wrapper_tr_fpll_pll_cal_busy_w [i]),//input
+            .xgmii_tx              ( tr_baser_wrapper_xgmii_tx_w             [i]),//input
+            .xgmii_tx_rdy          ( tr_baser_wrapper_xgmii_tx_rdy_w         [i]),//output
+            .xgmii_tx_clk          ( tr_baser_wrapper_xgmii_tx_clk_w         [i]),//output
+            .xgmii_tx_rst          ( tr_baser_wrapper_xgmii_tx_rst_w         [i]),//output
+            .xgmii_rx              ( tr_baser_wrapper_xgmii_rx_w             [i]),//output
+            .xgmii_rx_rdy          ( tr_baser_wrapper_xgmii_rx_rdy_w         [i]),//output
+            .xgmii_rx_clk          ( tr_baser_wrapper_xgmii_rx_clk_w         [i]),//output
+            .xgmii_rx_rst          ( tr_baser_wrapper_xgmii_rx_rst_w         [i]),//output
+            .rx_sync               ( tr_baser_wrapper_rx_sync_w              [i]) //output
         );
 
         assign xgmii_rx_clk_w [i] = tr_baser_wrapper_xgmii_rx_clk_w[i];
@@ -189,7 +197,7 @@ end
 
 //////////////////////////////////////////////////////////////////////////
 for (i=0;i<NUM_TR;i=i+1) begin : PCSRX
-    if (TYPE_TR[i]=="PMA") begin : PMA
+    if (TYPE_TR_PMA[i]==1'b1) begin : PMA
     assign pcs_rx_32b_clk_w      [i]= xgmii_rx_clk_w[i]; 
     assign pcs_rx_32b_rst_w      [i]= 0;
     assign pcs_rx_32b_pma_data_w [i]= tr_pma_wrapper_pma_rx_w[i];
@@ -209,7 +217,7 @@ end
 
 //////////////////////////////////////////////////////////////////////////
 for (i=0;i<NUM_TR;i=i+1) begin : PCSTX
-    if (TYPE_TR[i]=="PMA") begin : PMA
+    if (TYPE_TR_PMA[i]==1'b1) begin : PMA
     assign pcs_tx_32b_clk_w      [i]= xgmii_tx_clk_w[i];
     assign pcs_tx_32b_rst_w      [i]= 0;
     assign pcs_tx_32b_xgmii_tx_w [i]= (i==0) ? xgmii_retransmit_32b32b_fifo_0to0_tx_r : xgmii_retransmit_32b32b_fifo_0to1_tx_r;
@@ -253,6 +261,21 @@ xgmii_retransmit_32b32b_fifo xgmii_retransmit_32b32b_fifo_0to1_u
     .tx     (xgmii_retransmit_32b32b_fifo_0to1_tx_w    )
 );
 always @(posedge xgmii_retransmit_32b32b_fifo_0to1_clk_tx_w) xgmii_retransmit_32b32b_fifo_0to1_tx_r <= xgmii_retransmit_32b32b_fifo_0to1_tx_w;
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+assign xgmii_retransmit_32b64b_fifo_0to2_clk_rx_w = xgmii_rx_clk_w       [0];
+assign xgmii_retransmit_32b64b_fifo_0to2_rx_w     = pcs_rx_32b_xgmii_rx_w[0];
+assign xgmii_retransmit_32b64b_fifo_0to2_clk_tx_w = xgmii_tx_clk_w       [2];
+
+xgmii_retransmit_32b64b_fifo xgmii_retransmit_32b64b_fifo_0to2_u
+(
+    .clk_rx (xgmii_retransmit_32b64b_fifo_0to2_clk_rx_w),
+    .rx     (xgmii_retransmit_32b64b_fifo_0to2_rx_w    ),
+    .clk_tx (xgmii_retransmit_32b64b_fifo_0to2_clk_tx_w),
+    .tx     (xgmii_retransmit_32b64b_fifo_0to2_tx_w    )
+);
+always @(posedge xgmii_retransmit_32b64b_fifo_0to2_clk_tx_w) xgmii_retransmit_32b64b_fifo_0to2_tx_r <= xgmii_retransmit_32b64b_fifo_0to2_tx_w;
 ////////////////////////////////////////////////////////////////////////////////
 
 endmodule
